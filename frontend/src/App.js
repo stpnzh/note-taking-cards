34bb7153ba9c522f58fde499308b2efdea117109
Stephanie Zhu <slzzhu@gmail.com>
2020-10-29 15:08:57 -0700

    Connect frontend and backend with corsheaders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const items = await fetch('http://127.0.0.1:8000/notes/');
+    setData(await items.json());
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {data.map(i => <li>{i.description}</li>)}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
